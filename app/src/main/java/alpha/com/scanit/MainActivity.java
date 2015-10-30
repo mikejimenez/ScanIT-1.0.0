98e02cf4480f814d3c884a5fa7092299906d4f56
mikejimenez <tw1st3dm4st1c@gmail.com>
2015-10-29 19:07:42 -0700

    Small Updates
    Adding loadPreferemces()
    Adding LoadDatat()
    Trying to fix ActivityState data by using onStop()
    Adding savePreferences()
    Adding saveLog()
    Fixed formatting and rearranged code
    
    Fixing recent changes but most likely buggy! :(

--- app/src/main/java/alpha/com/scanit/MainActivity.java
+++ app/src/main/java/alpha/com/scanit/MainActivity.java
@@ -4,10 +4,10 @@ import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.text.Editable;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.Window;
@@ -20,39 +20,63 @@ import android.widget.TextView;
 import android.widget.Toast;
 import com.alpha.ZXing.android.IntentIntegrator;
 import com.alpha.ZXing.android.IntentResult;
+import android.util.Log;
 import java.util.Arrays;
 import java.util.List;
 
 
 public class MainActivity extends Activity {
 
+    public static final String PREFS_NAME = "Scan_IT";
+    ListView listView;
+    String TAG = "MAKE_BARCODES";
     // Private Strings
     private String[] log = new String[100];
     private String TypeG = "";
     private String TypeE = "";
     private Boolean Error = false;
     private TextView CounterTxt;
-    ListView listView;
     private Integer Counter = 0;
-    //String TAG = "MAKE_BARCODES";
+    private Integer size;
+    private String PauseState = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
+
         requestWindowFeature(Window.FEATURE_NO_TITLE); // for hiding title
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
+
         CreateListView();
+        LoadData();
 
         Button scanBtn = (Button) findViewById(R.id.scan_button);
         Button clrBtn = (Button) findViewById(R.id.clr_button);
         Button mnlBtn = (Button) findViewById(R.id.manual_button);
 
         CounterTxt = (TextView) findViewById(R.id.textView2);
-        CounterTxt.setText("0");
 
+        // if (PauseState.equals("Y")) { loadPreferences("Y","N"); loadPreferences("N","Y");}
+
+//        if (savedInstanceState != null && savedInstanceState.containsKey("array_")) {
+//            //log = savedInstanceState.getStringArray("log");
+//
+//            CharSequence CounterText = savedInstanceState.getCharSequence("Counter");
+//            Counter = Integer.parseInt(CounterText.toString());
+//
+//            int size = activityPreferences.getInt("array_size", 0);
+//            log = new String[size];
+//
+//            for (int i = 0; i < size; i++) {
+//                activityPreferences.getString("array_" + i, null);
+//            }
+//
+//
+//            CounterTxt.setText(CounterText);
+//        }
 
         scanBtn.setOnClickListener(
                 new View.OnClickListener() {
@@ -88,7 +112,8 @@ public class MainActivity extends Activity {
         );
     }
 
-    public void clrBtnData() {
+
+    private void clrBtnData() {
         SQLite db = new SQLite(this);
         db.deleteBarcodes();
         CreateListView();
@@ -96,7 +121,7 @@ public class MainActivity extends Activity {
         CounterTxt.setText("0");
     }
 
-    public void InputManual() {
+    private void InputManual() {
 
         LayoutInflater Manual = LayoutInflater.from(this);
         final View textEntryView = Manual.inflate(R.layout.manual_entry, null);
@@ -129,8 +154,7 @@ public class MainActivity extends Activity {
                             db.close();
                             CreateListView();
                             CounterTxt.setText(Integer.valueOf(Counter).toString());
-                        }
-                        else {
+                        } else {
                             db.addBarcodes(new Barcodes(Result, value.toString()));
                             Counter++;
                             db.close();
@@ -150,7 +174,10 @@ public class MainActivity extends Activity {
         alert.show();
     }
 
-    public void FormatEmail(String Output) {
+    /**
+     * FILTERS
+     */
+    private void FormatEmail(String Output) {
         // Display all bar codes
         // Log.d(TAG, "Reading - Display Results");
 
@@ -219,15 +246,14 @@ public class MainActivity extends Activity {
                 Output = Result;
                 log[i] = "\n" + Output + " / " + cn.getCompany();
                 i++;
-            }
-            else {
+            } else {
                 Error = true;
             }
         }
 
     }
 
-    public String Filter(String Number) {
+    private String Filter(String Number) {
 
         if (Number.length() == 34) {
             //
@@ -269,7 +295,7 @@ public class MainActivity extends Activity {
         return Number;
     }
 
-    public String FormatStringG(String Number) {
+    private String FormatStringG(String Number) {
         //
         // 32 Characters
         // Fedex Ground
@@ -279,7 +305,7 @@ public class MainActivity extends Activity {
         return ScanFromFedEXGO;
     }
 
-    public String FormatStringE(String Number) {
+    private String FormatStringE(String Number) {
         //
         // 34 Characters
         // Fedex Express
@@ -289,7 +315,7 @@ public class MainActivity extends Activity {
         return ScanFromFedEX;
     }
 
-    public String FormatStringManualE(String Number) {
+    private String FormatStringManualE(String Number) {
         // Manual Entry
         // 14 Characters
         // Fedex Express
@@ -299,7 +325,7 @@ public class MainActivity extends Activity {
         return ScanFromFedEXManual;
     }
 
-    public String FormatStringUPS(String Number) {
+    private String FormatStringUPS(String Number) {
         //
         // 18 Characters
         // UPS
@@ -309,7 +335,7 @@ public class MainActivity extends Activity {
         return ScanFromUPS;
     }
 
-    public String FormatStringManualUPS(String Number) {
+    private String FormatStringManualUPS(String Number) {
         // Manual Entry
         // 23 Characters
         // UPS
@@ -319,7 +345,7 @@ public class MainActivity extends Activity {
         return ScanFromManualUPS;
     }
 
-    public String FormatStringX(String Number) {
+    private String FormatStringX(String Number) {
         //
         // 22 Characters
         // Fedex Ground
@@ -332,7 +358,7 @@ public class MainActivity extends Activity {
         return Replace;
     }
 
-    public String FormatStringManualX(String Number) {
+    private String FormatStringManualX(String Number) {
         // 23 Characters
         // Fedex Ground
         // Format: XXXXXXX XXXXXXX XXXXXXX
@@ -343,7 +369,11 @@ public class MainActivity extends Activity {
         return Replace;
     }
 
-    public void emailResults() {
+    /**
+     * FILTERS
+     */
+
+    protected void emailResults() {
         Intent i = new Intent(Intent.ACTION_SEND);
         i.setType("message/rfc822");
         i.putExtra(Intent.EXTRA_SUBJECT, "Tracking Numbers");
@@ -365,7 +395,7 @@ public class MainActivity extends Activity {
         }
     }
 
-    public void CreateListView() {
+    private void CreateListView() {
 
         listView = (ListView) findViewById(R.id.listView);
 
@@ -386,7 +416,126 @@ public class MainActivity extends Activity {
         db.close();
     }
 
-    public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+//    @Override
+//    public void onRestoreInstanceState(Bundle savedInstanceState) {
+//
+//        // Always call the superclass so it can restore the view hierarchy
+//        super.onRestoreInstanceState(savedInstanceState);
+//
+//
+//        TextView CountData = (TextView) findViewById(R.id.textView2);
+//        log = savedInstanceState.getStringArray("log");
+//
+//        // Restore state members from saved instance
+//        Log.d(TAG, log.toString());
+//        CharSequence CounterText = savedInstanceState.getCharSequence("Counter");
+//        CountData.setText(CounterText);
+//        Counter = Integer.parseInt(CounterText.toString());
+//    }
+
+    protected void loadPreferences(String L, String D) {
+        SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
+
+        if (settings != null && L.equals("Y")) {
+            size = settings.getInt("array_size", 0);
+            log = new String[size];
+
+            for (int i = 0; i < size; i++) {
+                log[i] = settings.getString("array_" + i, null);
+            }
+        }
+        if (settings != null && D.equals("Y")) {
+            String value = settings.getString("counter", null);
+            CounterTxt = (TextView) findViewById(R.id.textView2);
+            CounterTxt.setText(value);
+            Counter = Integer.parseInt(value);
+        }
+    }
+
+    protected void LoadData() {
+
+        SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
+
+        if (settings != null && settings.contains("paused")) {
+            SharedPreferences.Editor SharedEditor = settings.edit();
+            loadPreferences("Y", "N");
+            loadPreferences("N", "Y");
+            SharedEditor.remove("paused");
+            SharedEditor.apply();
+            Log.i(TAG, "Loaded");
+        } else {
+            CounterTxt = (TextView) findViewById(R.id.textView2);
+            CounterTxt.setText("0");
+        }
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        saveLog();
+        savePreferences();
+    }
+
+    protected void savePreferences() {
+        SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
+        SharedPreferences.Editor SharedEditor = settings.edit();
+
+        String value = CounterTxt.getText().toString();
+        String value2 = "Y";
+
+        SharedEditor.putInt("array_size", log.length);
+        SharedEditor.putString("counter", value);
+        SharedEditor.putString("paused", value2);
+        SharedEditor.commit();
+
+    }
+
+    protected void saveLog() {
+        SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
+        SharedPreferences.Editor SharedEditor = settings.edit();
+
+        for (int i = 0; i < log.length; i++) {
+            SharedEditor.putString("array_" + i, log[i]);
+            SharedEditor.commit();
+        }
+    }
+//    @Override
+//    protected void onResume() {
+//        super.onResume();
+//
+//        if (Pause.contains("Y")) {
+//            SharedPreferences activityPreferences;
+//            activityPreferences = getPreferences(Activity.MODE_PRIVATE);
+//
+//           // String CounterData = activityPreferences.getString("Counter", "");
+//            //CounterTxt.setText(CounterData);
+//            //Log.i(TAG, CounterData.toString());
+//
+//            size = activityPreferences.getInt("array_size", 0);
+//            log = new String[size];
+//
+//            for (int i = 0; i < size; i++) {
+//                activityPreferences.getString("array_" + i, null);
+//            }
+//            Pause = "N";
+//        }
+//    }
+//    @Override
+//    public void onSaveInstanceState(Bundle savedInstanceState) {
+//
+//        TextView CountData = (TextView) findViewById(R.id.textView2);
+//        CharSequence CounterText = CountData.getText();
+//
+//        //savedInstanceState.putString("log", Arrays.toString(log));
+//
+//        savedInstanceState.putCharSequence("Counter", CounterText);
+//
+//      //  Log.d(TAG, "SAVE: " + log.toString());
+//
+//        super.onSaveInstanceState(savedInstanceState);
+//    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
         IntentResult scanningResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, intent);
 
         if (resultCode == RESULT_OK) {
@@ -423,11 +572,10 @@ public class MainActivity extends Activity {
                         }
                     });
             alert.show();
+        } else if (resultCode == RESULT_CANCELED) {
+            Toast toast = Toast.makeText(getApplicationContext(),
+                    "No scan data received!", Toast.LENGTH_SHORT);
+            toast.show();
         }
-        else if (resultCode == RESULT_CANCELED) {
-                 Toast toast = Toast.makeText(getApplicationContext(),
-                          "No scan data received!", Toast.LENGTH_SHORT);
-                  toast.show();
-              }
     }
 }
